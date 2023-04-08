e439755ceddaf745e30aed6bbcbe74343c9131f0
Paul Cabeza <ovidio.cabeza@gmail.com>
2023-04-07 23:01:13 -0600

    Added Collection to productserializer as primarykeyfield=number of collection

--- store/serializers.py
+++ store/serializers.py
@@ -1,12 +1,19 @@
 from decimal import Decimal
 from rest_framework import serializers
-from .models import Product
+from .models import Product, Collection
+
+class CollectionSerializer(serializers.Serializer):
+    pass
+    
 
 class ProductSerializer(serializers.Serializer):
     id = serializers.IntegerField()
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
+    collection = serializers.PrimaryKeyRelatedField(
+        queryset = Collection.objects.all()
+    )
 
     def get_price_with_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
