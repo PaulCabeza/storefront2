7b3a55756df92a84438d4ed9746deb9909be9095
Paul Cabeza <ovidio.cabeza@gmail.com>
2023-04-07 23:17:57 -0600

    Finish adding a field for related model serialized

--- store/serializers.py
+++ store/serializers.py
@@ -3,7 +3,8 @@ from rest_framework import serializers
 from .models import Product, Collection
 
 class CollectionSerializer(serializers.Serializer):
-    pass
+    id = serializers.IntegerField()
+    title = serializers.CharField(max_length=255)
     
 
 class ProductSerializer(serializers.Serializer):
@@ -11,9 +12,7 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
-    collection = serializers.PrimaryKeyRelatedField(
-        queryset = Collection.objects.all()
-    )
+    collection = CollectionSerializer()
 
     def get_price_with_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
