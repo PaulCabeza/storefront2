06eb3136db6c193a2d5eca630e95ddab54674760
Paul Cabeza <ovidio.cabeza@gmail.com>
2023-04-13 17:17:00 -0600

    Added serializer collection relationship

--- store/serializers.py
+++ store/serializers.py
@@ -12,7 +12,7 @@ class ProductSerializer(serializers.Serializer):
     title = serializers.CharField(max_length=255)
     price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
     price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
-    collection = CollectionSerializer()
+    collection = CollectionSerializer()    
 
     def get_price_with_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
