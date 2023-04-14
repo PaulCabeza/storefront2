6196134109ebe3007ed6bcbbdf40433c32cd2b9c
Paul Cabeza <ovidio.cabeza@gmail.com>
2023-04-14 10:34:55 -0600

    Added POSt method to product_list, to prepare it to save an object

--- store/views.py
+++ store/views.py
@@ -9,11 +9,15 @@ from .models import Product
 from .serializers import ProductSerializer
 
 # Create your views here.
-@api_view()
+@api_view(['GET', 'POST'])
 def product_list(request):
-    queryset = Product.objects.select_related('collection').all()
-    serializer = ProductSerializer(queryset, many=True)
-    return Response(serializer.data)
+    if request.method == 'GET':
+        queryset = Product.objects.select_related('collection').all()
+        serializer = ProductSerializer(queryset, many=True)
+        return Response(serializer.data)
+    elif request.method == 'POST':
+        serializer = ProductSerializer(data=request.data)
+        return Response('ok')
 
 @api_view()
 def product_detail(request, id):
