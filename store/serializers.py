56e57605826b03cb9ac32937328a3ba16f2b9bca
Paul Cabeza <ovidio.cabeza@gmail.com>
2023-04-14 10:26:13 -0600

    Added modelserializer class to serializer

--- store/serializers.py
+++ store/serializers.py
@@ -2,17 +2,19 @@ from decimal import Decimal
 from rest_framework import serializers
 from .models import Product, Collection
 
-class CollectionSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
+class CollectionSerializer(serializers.ModelSerializer):    
+    class Meta:
+        model = Collection
+        fields = ['id', 'title']
+
     
 
-class ProductSerializer(serializers.Serializer):
-    id = serializers.IntegerField()
-    title = serializers.CharField(max_length=255)
-    price = serializers.DecimalField(max_digits=6, decimal_places=2, source='unit_price')
-    price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')
-    collection = CollectionSerializer()    
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Product
+        fields = ['id', 'title', 'unit_price', 'price_with_tax', 'collection']
+
+    price_with_tax = serializers.SerializerMethodField(method_name='get_price_with_tax')    
 
     def get_price_with_tax(self, product: Product):
         return product.unit_price * Decimal(1.1)
